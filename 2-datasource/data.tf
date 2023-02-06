d717373ecaae8e47ba5377f7f5d681fac440b139
raviteja <ravitejatfcdemo@gmail.com>
2023-02-06 14:38:06 +0530

    adding Jenkinsfile

--- 2-datasource/data.tf
+++ 2-datasource/data.tf
@@ -5,7 +5,20 @@ data "aws_ami" "example" {
 }
 
 # most_recent, name_regex, owners -- Arguements
+# arguements are the input we provide to data
 
 output "ami" {
   value = data.aws_ami.example
+}
+
+# attributes are the information that it is going to fetch by data
+# attributes are the output we get from data
+# It will fetch data, the data side key-words is called attributes
+
+data "aws_instance" "instance" {
+  instance_id = "i-0d1569df18c7d5857"
+}
+
+output "instance" {
+  value = data.aws_instance.instance.private_ip
 }
