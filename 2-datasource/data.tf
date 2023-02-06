a4ebc49b9a02642650e9f7d73aadb810e195bf1b
raviteja <ravitejatfcdemo@gmail.com>
2023-02-06 14:16:07 +0530

    adding Jenkinsfile

--- 2-datasource/data.tf
+++ 2-datasource/data.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "ami" {
+data "aws_ami" "example" {
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
@@ -7,5 +7,5 @@ data "aws_ami" "ami" {
 # most_recent, name_regex, owners -- Arguements
 
 output "ami" {
-  value = "data.aws_ami.ami"
+  value = data.aws_ami.example
 }
