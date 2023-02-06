2e748dc33797da526f23503e8437422038d8eaee
raviteja <ravitejatfcdemo@gmail.com>
2023-02-06 15:31:02 +0530

    Terraform

--- 3-variables/vars.tf
+++ 3-variables/vars.tf
@@ -5,3 +5,42 @@ variable "sample" {
 output "variable" {
   value = var.sample
 }
+
+
+# default variable type
+variable "Default" {
+  default = "Hello"
+}
+
+output "variable1" {
+  value = var.Default
+}
+
+
+# list variable
+# index 0,1,2....
+variable "List" {
+  default = [
+    100,
+    "abc",
+    true
+  ]
+}
+
+output "variable2" {
+  value = var.List.2
+}
+
+
+#map variables
+variable "Map" {
+  default = {
+    number = 100
+    string = "abc"
+    boolean = true
+  }
+}
+
+output "variable3" {
+  value = var.Map.string
+}
