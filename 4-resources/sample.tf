b5bb2e48eb2529ab0e3e57199ae82b07bf866bca
raviteja <ravitejatfcdemo@gmail.com>
2023-02-07 11:34:48 +0530

    Terraform

---
+++ 4-resources/sample.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+# we require ami, so we are using this datasource
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
